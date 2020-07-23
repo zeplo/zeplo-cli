b5faececb9ddf7f792959b694ac99250f47bd814
Calum Moore <calum.moore@gmail.com>
2020-07-23 23:48:52 +0100

    Clear-up unused tests

--- packages/cli/src/cmds/workspaces/__tests__/list.spec.js
+++ packages/cli/src/cmds/workspaces/__tests__/list.spec.js
@@ -1,10 +1,8 @@
 import yargs from 'yargs'
 import { handler } from '../list'
 import output from '../../../helpers/output'
-import graphql from '../../../helpers/graphql'
 // import createTable from '../../../helpers/table'
 
-jest.mock('../../../helpers/graphql')
 jest.mock('../../../helpers/output')
 jest.mock('../../../helpers/table')
 
@@ -14,8 +12,6 @@ describe('cmds/spaces.spec', () => {
   })
 
   test('spaces list with no spaces', async () => {
-    graphql.mockResolvedValue({ spaces: [] })
-
     const args = yargs.parse(['spaces', '--token', 'x123'])
 
     await handler(args)
--- packages/cli/src/helpers/__tests__/util.spec.js
+++ packages/cli/src/helpers/__tests__/util.spec.js
@@ -1,4 +1,4 @@
-import { forEach, matchArr } from '../util'
+import { matchArr } from '../util'
 
 describe('util.spec', () => {
   describe('matchArr', () => {
@@ -10,34 +10,6 @@ describe('util.spec', () => {
     test('match should return undefined for no match', () => {
       const arr = ['abc1', 'def', 'abc2']
       expect(matchArr(arr, /zyx/)).toEqual(undefined)
-    })
-  })
-
-  describe('forEach', () => {
-    test('loops through provided array passing value', () => {
-      const arr = ['a', 'b', 'c']
-      const out = []
-      forEach(arr, (val) => { out.push(val) })
-      expect(out).toEqual(arr)
-    })
-
-    test('loops through provided array passing index', () => {
-      const arr = ['a', 'b', 'c']
-      const out = []
-      forEach(arr, (val, index) => { out.push(index) })
-      expect(out).toEqual([0, 1, 2])
-    })
-
-    test('returns value of first undefined loop', () => {
-      const arr = ['a', 'b', 'c']
-      const out = forEach(arr, val => val)
-      expect(out).toEqual('a')
-    })
-
-    test('returns null if loop exhausted without return value', () => {
-      const arr = ['a', 'b', 'c']
-      const out = forEach(arr, () => undefined)
-      expect(out).toEqual(null)
     })
   })
 })
